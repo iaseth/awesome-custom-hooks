67759ccd1ba6886ab8460eab31cc3f3ba4f45822
Ankur Seth <iankurseth@gmail.com>
2023-04-10 13:29:35 +0530

    Added decrementCount and resetCount to useCounter

--- src/useCounter.ts
+++ src/useCounter.ts
@@ -4,6 +4,10 @@ import React from 'react';
 
 export const useCounter = (intialState: number=0) => {
 	const [count, setCount] = React.useState(intialState);
+
 	const incrementCount = () => setCount((x: number) => x+1);
-	return [count, incrementCount];
+	const decrementCount = () => setCount((x: number) => x-1);
+	const resetCount = () => setCount(intialState);
+
+	return [count, incrementCount, decrementCount, resetCount];
 };
