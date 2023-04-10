92f64f3edabf770ca12a5c02fcf7d1d278396f8f
Ankur Seth <iankurseth@gmail.com>
2023-04-10 18:11:06 +0530

    Added python class CustomHook to generate script

--- generate.py
+++ generate.py
@@ -2,19 +2,32 @@ import os
 import sys
 
 
-def get_hook_paths():
+class CustomHook:
+	def __init__(self, entry):
+		self.entry = entry
+		self.name = entry.split("/")[-1]
+		self.path = f"{entry}.ts"
+		self.fullpath = f"src/{entry}.ts"
+
+	def print(self):
+		print(self)
+
+	def __str__(self):
+		return f"CustomHook \"{self.name}\" ({self.fullpath})"
+
+
+def get_hooks():
 	hooklist_text = open("hooklist.txt").read()
 	hooklist_lines = [x.strip() for x in hooklist_text.split("\n")]
 	hook_paths = [x for x in hooklist_lines if x]
-	return hook_paths
+	hooks = [CustomHook(x) for x in hook_paths]
+	return hooks
 
 
-def generate_readme():
-	hook_paths = get_hook_paths()
+def generate_readme(hooks):
 	HOOK_LIST_MD = ""
-	for hook_path in hook_paths:
-		hook = hook_path.split("/")[-1]
-		HOOK_LIST_MD += f"* `{hook}`\n"
+	for hook in hooks:
+		HOOK_LIST_MD += f"* `{hook.name}`\n"
 
 	preadme_text = open("PREADME.md").read()
 	readme_text = preadme_text.replace("[[HOOK_LIST_MD]]", HOOK_LIST_MD)
@@ -23,10 +36,9 @@ def generate_readme():
 		print(f"saved: README.md")
 
 
-def create_modules():
-	hook_paths = get_hook_paths()
-	for hook_path in hook_paths:
-		filepath = f"src/{hook_path}.ts"
+def create_modules(hooks):
+	for hook in hooks:
+		filepath = hook.fullpath
 		if os.path.isfile(filepath):
 			print(f"Exists: {filepath}")
 		else:
@@ -35,18 +47,15 @@ def create_modules():
 				print(f"Created: {filepath}")
 
 
-def generate_index_ts():
-	hook_paths = get_hook_paths()
+def generate_index_ts(hooks):
 	text = ""
-	for hook_path in hook_paths:
-		hook_name = hook_path.split("/")[-1]
-		text += f"import {{ {hook_name} }} from './{hook_path}';\n"
+	for hook in hooks:
+		text += f"import {{ {hook.name} }} from './{hook.entry}';\n"
 
 	text += "\n\n\n"
 	text += "const Awesome = {\n"
-	for hook_path in hook_paths:
-		hook_name = hook_path.split("/")[-1]
-		text += f"\t{hook_name},\n"
+	for hook in hooks:
+		text += f"\t{hook.name},\n"
 
 	text += "};\n\n"
 	text += "export default Awesome;\n"
@@ -58,15 +67,17 @@ def generate_index_ts():
 
 
 def main():
+	hooks = get_hooks()
+
 	args = sys.argv[1:]
 	command = None if len(args) == 0 else args[0].lower()
 
 	if command == "readme":
-		generate_readme()
+		generate_readme(hooks)
 	elif command == "create":
-		create_modules()
+		create_modules(hooks)
 	elif command == "ts":
-		generate_index_ts()
+		generate_index_ts(hooks)
 	else:
 		print("No command provided.")
 
